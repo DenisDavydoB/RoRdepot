e5ed68eb93e6424931ea9c823b254c7fe9e75f74
Denis Davydov <denis.davydov@kodep.ru>
2021-08-27 15:34:09 +0300

    10E1

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,19 @@
 class StoreController < ApplicationController
+
+  def increment_counter
+    if session[:counter].nil? 
+      session[:counter] = 0
+    end 
+    session[:counter] += 1
+  end 
+  
+  def reset_counter
+    session[:counter] = 0
+  end  
+
   def index
     @products = Product.order(:title)
+    @count = increment_counter
+    @shown_message = "You've been here #{@count} times" if @count >5
   end
 end
